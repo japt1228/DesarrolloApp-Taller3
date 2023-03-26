5e8975ca5c96d14cee090edb3a02ddda505b6e8e
Julian Palacio <japt94@gmail.com>
2023-03-25 21:20:50 -0500

    Creacion de las clases

---
+++ Usuarios.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1
+{
+    public class Usuarios
+    {
+        public string Nombre {get; set; }
+
+        public int Cedula { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Telefono { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+
+    }
+}
